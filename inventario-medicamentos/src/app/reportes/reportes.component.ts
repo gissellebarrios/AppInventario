d35d22659b9b7525d9cdfb701a0729e086b52cb0
Gisselle B <gissellebarrios915@gmail.com>
2024-10-05 15:54:22 -0500

    se avanzo

--- inventario-medicamentos/src/app/reportes/reportes.component.ts
+++ inventario-medicamentos/src/app/reportes/reportes.component.ts
@@ -1,23 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { saveAs } from 'file-saver';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-reportes',
   templateUrl: './reportes.component.html',
   styleUrls: ['./reportes.component.css']
 })
-export class ReportesComponent {
-  periodo = 'diario';
-  formato = 'json';
+export class ReportesComponent implements OnInit {
+  movimientos: any[] = [];
+  empresa_nombre: string = 'Nombre de la empresa';
+  fechaReporte: Date = new Date();
 
   constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.obtenerMovimiento();
+  }
     
-    generarReporte(){
-      const url = 'http://localhost:8000/api/reporte/$(this.periodo)7?formato=$(this.formato)';
-      this.http.get(url,{ responseType:'blob' }).subscribe(blob =>{
-        const filename = this.formato === 'json' ? 'reporte_inventario.json':'reporte_inventario.${this.formato}';
-        saveAs(blob,filename);
+  obtenerMovimiento(): void {
+    this.http.get<any>('http://127.0.0.1:8000/api/movimientos/')
+    .subscribe(
+      (data) =>{
+        this.movimientos = data.results;
+      },
+      (error) => {
+        console.error('Error al obtener los movimientos;', error);
+      }
+    );
+  }
+
+  generarPDF(): void {
+    const pdf = new jsPDF();
+    const contenido = document.getElementById('contenido-reporte'); // ID del div que contiene la tabla
+
+    if (contenido) {
+      contenido.style.display = 'block';
+      html2canvas(contenido).then(canvas => {
+        console.log(canvas);
+        const imgData = canvas.toDataURL('image/png');
+        const imgWidth = 190; // Ancho de la imagen
+        const pageHeight = pdf.internal.pageSize.height;
+        const imgHeight = (canvas.height * imgWidth) / canvas.width;
+        let heightLeft = imgHeight;
+
+        let position = 0;
+
+        pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+
+        while (heightLeft >= 0) {
+          position = heightLeft - imgHeight;
+          pdf.addPage();
+          pdf.addImage(imgData, 'PNG', 10, position, imgWidth, imgHeight);
+          heightLeft -= pageHeight;
+        }
+
+        pdf.save('reporte_movimientos.pdf');
+        contenido.style.display = 'none';
       });
     }
+  }
 }
